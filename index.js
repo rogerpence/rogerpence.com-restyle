8f294cad882d4de3074d0e6e699748ab52aa7417
Roger Pence <roger.pence@gmail.com>
2019-07-06 00:28:23 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+autoboundEventHandlers.addEventHandler('toggleNavMenu', (e) => {
+    console.log('clicked');
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('is-active');
+
+    // const links = document.querySelector('.mobile-info-panel');
+    // links.classList.toggle('mobile-info-panel-show');
+});
+
+assignAutoboundEventHandlers();
